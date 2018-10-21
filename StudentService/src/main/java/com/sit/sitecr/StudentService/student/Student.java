a349e81ede89573b012c9627eebfb0a4123b0249
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-10-21 16:14:32 +0700

    [ADD] Student model

---
+++ StudentService/src/main/java/com/sit/sitecr/StudentService/student/Student.java
@@ -0,0 +1,93 @@
+package com.sit.sitecr.StudentService.student;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "students")
+public class Student implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long studentId;
+    @NotBlank
+    private String studentName;
+    @NotBlank
+    private String studentSurname;
+    @NotBlank
+    private String studentDepartment;
+    @NotBlank
+    private String studentFaculty;
+    @NotBlank
+    private int studentYear;
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+
+    public Student() {
+    }
+
+    public long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentSurname() {
+        return studentSurname;
+    }
+
+    public void setStudentSurname(String studentSurname) {
+        this.studentSurname = studentSurname;
+    }
+
+    public String getStudentDepartment() {
+        return studentDepartment;
+    }
+
+    public void setStudentDepartment(String studentDepartment) {
+        this.studentDepartment = studentDepartment;
+    }
+
+    public String getStudentFaculty() {
+        return studentFaculty;
+    }
+
+    public void setStudentFaculty(String studentFaculty) {
+        this.studentFaculty = studentFaculty;
+    }
+
+    public int getStudentYear() {
+        return studentYear;
+    }
+
+    public void setStudentYear(int studentYear) {
+        this.studentYear = studentYear;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
