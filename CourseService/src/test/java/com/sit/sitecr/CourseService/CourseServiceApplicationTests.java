620797b506028f21fe4bb92bc3129dbfa67f8a1b
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-10-21 16:39:15 +0700

    [init] CourseService

---
+++ CourseService/src/test/java/com/sit/sitecr/CourseService/CourseServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sit.sitecr.CourseService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CourseServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
