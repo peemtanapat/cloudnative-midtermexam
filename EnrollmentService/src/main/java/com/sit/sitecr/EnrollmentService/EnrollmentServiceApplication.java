83d75b4264d6ad266e49e767c84e008748c927e0
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-10-21 18:04:27 +0700

    [init] EnrollmentService

---
+++ EnrollmentService/src/main/java/com/sit/sitecr/EnrollmentService/EnrollmentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sit.sitecr.EnrollmentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollmentServiceApplication.class, args);
+	}
+}
