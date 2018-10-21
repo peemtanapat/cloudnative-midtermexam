6f288ec98d43bfaad2d93d8b0fee2aa128899869
Tanapat128 <tanapat.pm@mail.kmutt.ac.th>
2018-10-21 16:14:03 +0700

    [Init] StudentService

---
+++ StudentService/src/main/java/com/sit/sitecr/StudentService/StudentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sit.sitecr.StudentService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceApplication.class, args);
+	}
+}
